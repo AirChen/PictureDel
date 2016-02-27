e46424056ed6520db70334fe19ca1651eccdbfdf
Air chen <renzhichen@air-chendemac-mini.local>
2016-02-27 21:59:44 +0800

    Initial Commit

---
+++ AIRPicDeal/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AIRPicDeal
+//
+//  Created by Air_chen on 16/2/27.
+//  Copyright © 2016年 Air_chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AIRPicDeal/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AIRPicDeal
+//
+//  Created by Air_chen on 16/2/27.
+//  Copyright © 2016年 Air_chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
